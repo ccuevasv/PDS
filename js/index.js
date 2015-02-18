b1fea03fb53173389c37b8306f72a0701c6bfb10
ccuevasv <gerard_1093@hotmail.com>
2015-02-18 11:14:11 -0600

    Cambio nic-btn
    Se agregó funcionalidad a boton terminar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
+var actCount = 1;
 $(function(){
-
+	$('[data-toggle="popover"]').popover()
 	var actNum = 1;
-	var actCount = 1;
+	
 	var newAct = $('.tr-act').html();
 	$('.tr-act').remove();
 
@@ -11,36 +12,42 @@ $(function(){
 
 	    $('.tbody-act').append("<tr>"+newAct.replace(/1/g,actCount)+"</tr>");
 
-	    var dt = new Date();
-	    var date = dt.toISOString().slice(0,10);
-	    var time = dt.getHours()+":"+dt.getMinutes();
-	    $('#fechaAct-'+actCount).text(date);
-	    $('#horaInAct-'+actCount).text(time); 
+		$('#fechaAct-'+actCount).text(getCurrentDate());
+	    $('#horaInAct-'+actCount).text(getTime()); 
 	    $('#tipoAct-'+actCount).text($('#planAct-'+actNum).text());
-	    //alert(actNum);
 	    
 	    actCount++;
-	  });
-
-
-	$('#btn-terminar-1').on('click', function (e) {
-			alert(e.target.id);
-	    $('#avance-'+actCount-1).text('100%');
-		});
+	});
 
 	$('#tblGrid').on('click','tr', function () {
 		var trId = $(this).attr('id');
 		actNum = trId.slice(-1);
 	    $(this).toggleClass("clicked");
-	 });
-
-})
-
-function advance(){
-
-	var pId = $(this).attr("id");
-	var id = pId.slice(-1);
-	alert(id);
-	$('#avance-'+id).text('100%'); 
-
-}
+	});
+	 
+	$(document).on('click','button',function(event) {
+		var id = $(this).attr('id');
+		var btnTer = "btn-terminar-"
+		var btnPaus = "btn-pausar-"
+		var actNo = actCount-1;
+		
+		if ( id == btnTer + actNo){
+			$('#avance-'+actNo).text('100%');
+			$('#horaFinAct-'+actNo).text(getTime());
+		}
+	
+	});
+	
+	function getCurrentDate(){
+		var dt = new Date();
+	    var date = dt.toISOString().slice(0,10);
+		return date;
+	}
+	
+	function getTime(){
+		var dt = new Date();
+		var mins = ("0"+dt.getMinutes()).slice(-2)
+	    var time = dt.getHours()+":"+mins;
+		return time;
+	}
+});
